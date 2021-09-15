2dbd7b3ef9326ba2fe50b950602f9f814728486c
Joann Pan <joannlpan@gmail.com>
2021-09-15 16:42:38 -0400

    add namespacing to routes

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,13 @@
 class Account < ApplicationRecord
-   has_many :transactions
-   validates :name, :balance, presence: true
+
+    namespace :api do 
+        namespace :v1 do
+            has_many :transactions
+            validates :name, :balance, presence: true
+        end
+    end
 end
+
+
+#adding a namespace will change the routes 
+#http://localhost:3000/transitions -> http://localhost:3000/api/v1/transitions 
