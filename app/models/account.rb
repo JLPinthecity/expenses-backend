f6a551e6dfc74a38e27345e43b7721dd1462c7f3
Joann Pan <joannlpan@gmail.com>
2021-09-17 18:33:43 -0400

    add ruby method to account model to prevent withdrawals from account with no or low balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,8 +2,29 @@ class Account < ApplicationRecord
     has_many :transactions
     validates :name, :balance, presence: true
 
+
+    def update_balance(transaction)
+        if transaction.kind == 'deposit'
+            self.balance = self.balance + transaction.amount
+            self.save
+        elsif transaction.kind == 'withdraw'
+            if self.balance >= transaction.amount
+            self.balance = self.balance + transaction.amount
+            self.save
+            else
+                return "Balance too low."
+            end
+        end
+    end
+
 end
 
 
 #adding a namespace will change the routes 
 #http://localhost:3000/transitions -> http://localhost:3000/api/v1/transitions 
+
+
+#EXAMPLE
+#t = Transaction.create(amount:10, kind: 'deposit')
+#a = Account.first
+#a.update_balance(t)
